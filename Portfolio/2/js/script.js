eb6db11ebb94f735cbda1478ba559191265d5d29
Lakshmanan <60023855+KumarLakshmanan@users.noreply.github.com>
2020-11-14 01:52:52 -0800

    Add files via upload

---
+++ Portfolio/2/js/script.js
@@ -0,0 +1,56 @@
+
+// Preloader
+
+$(window).on("load" , function () {
+    function HideLoader() {
+        setTimeout(() => {
+            $("#Loader").fadeOut(100)
+        }, 500);
+    }
+    HideLoader();
+})
+
+// Navbar
+
+$(window).on("scroll load" , function () {
+    if($(".navbar").offset().top > 40){
+        $(".navbar").addClass("shrink");
+        $(".Gototop").css("visibility" , "visible");
+    }
+    else{
+        $(".navbar").removeClass("shrink");
+        $(".Gototop").css("visibility" , "hidden");
+    }
+})
+
+// Portfolio Filter
+
+// Active Class
+$(".Filter_List .inline-list li").on("click" , function () {
+    var FilterClass = $(this).attr("filter");
+    console.log(FilterClass)
+    if (!$(this).hasClass("active")) {
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+    }
+    $(".Portfolio_projects .row .col-md-6").each(function() {
+        if(FilterClass == "All"){
+            $(this).fadeIn()
+        }
+        else{
+            if($(this).hasClass(FilterClass)){
+                $(this).fadeIn();
+            }
+            else{
+                $(this).hide();
+            }
+        }
+    })
+})
+
+
+// Toggle Active class
+
+$(".collapse_menu").on("click" , function(){
+    $(this).toggleClass("active");
+})
