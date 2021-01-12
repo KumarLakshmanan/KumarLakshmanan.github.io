a3d2235d47ec6cfcaa00bf2bc3db24fe94de5030
Lakshmanan <60023855+KumarLakshmanan@users.noreply.github.com>
2021-01-12 06:48:15 -0800

    Create adsbygoogle.js

---
+++ mylibraries/adsbygoogle.js
@@ -0,0 +1,13 @@
+// I use this code to detect adblockers on Browser
+
+var isAdsDisplayed = true;
+
+
+
+// <!-- Add this code on Your Index.html file -->
+// <script src="https://kumarlakshmanan.github.com/mylibraries/adsbygoogle.js"></script>
+// <script>
+//   if(window.isAdsDisplayed === undefined ) {
+//     // AdBlock is enabled. Show message or track custom data here
+//   }
+// </script>
